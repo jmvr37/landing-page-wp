6bbe930dd70e89898918664e9ad74a6f8a29b714
jose <jmvr37@hotmail.com>
2020-03-21 02:42:36 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class=flex-header>
+		
+				<div>
+					<p>MONTHLY INCOME
+					<br />
+					MORTGAGE TRUST</p></div>
+					<div class="title-footer">
+					<p class="initials">JZ</p>
+					<p>JUDY ZAVAZAL</p>
+				</div>
+				
+						</div>
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Template name: front-page
+ * The main template file.
+ *
+ */ get_header(); ?>
+
+	<div id="primary" class="content-area-front">
+		<main id="main" class="site-main" role="main">
+
+<div class="text-container">
+    <h2>HERE’S HOW TO BULLET-PROOF
+</h2>
+<div class="sub-title">
+<h2>YOUR RETIREMENT INCOME...</h2>
+</div>
+</div>
+
+<div class="main-container">
+
+    <div class="main-text-container">
+    <p> <span class="italic">WITHOUT</span> Low Interest Government Bonds and Blue Chip Stocks,</p>
+    <p>Absurdly Risky Penny Stocks, all while enjoying a Tax Deduction!</p>
+    </div>
+
+    <div class="main-text-container">
+
+<div class="form">
+        <?php echo do_shortcode('[ninja_form id=3]') ?>
+            </div>
+
+            
+</div>
+
+     
+
+
+
+
+
+<div class="content-article">
+<div class="content-sheet">
+<p class="head-title"><span style="font-weight: bold">ATTN:</span> INVESTORS</p>
+
+<div class="text-container-sheet">
+
+<h1>ARE YOU AT RISK OF BORING, SLOW RETURNS OR</h1>
+        <div class="sub-title">
+            <h2>LOSING IT ALL IN RISKY STOCKS??</h2
+                    </div>
+</div>
+
+
+<div class="content-text">
+<p><span class="italic">Hi, I’m Judy Zavazal,</span> a certified agent of Tricity Group., and I’m here to show you a better way.</p>
+ 
+<p>I’ve got a new investment vehicle for you, the Tricity Mortgage Investment Fund, that’s going to help
+you specifically with getting 8% annual interest on your RRSP savings.</p>
+
+<p>If you’ve been struggling with deciding between risking your money in the stock market or accepting low interest government bonds, then you need this fund in your portfolio because, among other
+things, it allows you to invest directly into mortgages, a super secure and high interest loan market
+that has a track record of 8% annual returns per year over the last 8 years.</p>
+
+<p>Just fill out the contact form anywhere on this page. Let me know your name and email address,
+and I’ll get in touch right away to help you invest in the Tricity Mortgage Investment Fund.</p>
+
+<p>Now, you’re probably wondering, what’s the catch?</p>
+
+<p>There’s no catch. I’m doing this because I know I have something amazing and helpful, so I want to
+share it with as many people as I can.</p>
+
+<p>All I need from you is your email address, and we can get you earning huge returns on your investments!
+You don’t need me to tell you that compound interest is a long term game - the longer you wait, the
+more earnings you miss out on, so get in touch today!</p>
+
+<p>Even if we get on the phone and you don’t love the terms, there’s no obligation to buy - but this
+really is in your best interest.</p>
+
+<p>So, just to recap, you’re getting preferred access to the Tricity Mortgage Investment Fund, that will
+help you get 8% annual interest on your RRSP savings and save big on taxes.</p>
+
+<p>Just fill out the form, let me know your email address, and I’ll get in touch instantly!</p>
+
+</div>
+
+<div class="btn-container">
+<button>YES! I'M READY TO EARN 8% ANNUAL INTEREST RETURNS</button></div>
+</div>
+</div>
+</div>
+
+<div class="content-section">
+<div class="content-section-sheet">
+    <div class="text-container-paragraph">
+    <h2>I UNDERSTAND THAT WHEN I ACT NOW, I GET PREFERRED ACCESS TO THE
+</h2>
+<div class="sub-title">
+<h2>TRICITY MORTGAGE INVESTMENT FUND, WHICH INCLUDES:</h2>
+</div>
+<div class="content-text">
+<p>·Higher interest than government bonds... which helps me earn more money... which means retire
+faster and richer </p>
+
+<p>· RRSP eligiblity, which lets me write off these investments...
+which means I can save on my tax bill</p>
+
+<p>· Extraordinary security compared to most stocks... which means I assume less risk than I would by
+investing in the stock market</p>
+
+<p>· Avoid the boring, blah-blah returns of blue chip stocks and anxiously checking the Business News
+Network</p>
+</div>
+
+</div>
+
+</div>
+<div class="content-section-sheet">
+    <div class="text-container-paragraph">
+    <h2>100% GUARANTEED SAFE INVESTMENT INQUIRY
+</h2>
+
+<div class="content-text">
+<p>I believe in this product, so here’s my promis to you: </p>
+
+<p>The 100% Guaranteed Safe Investment Inquiry.</p>
+
+<p>This means that if I don’t show you exactly how to have retirement security... if I don’t take you by the
+hand, step-by-step, and show you how you can have higher rates of interest on your RRSP income...
+or if I fail to help you avoid the risk of the stock market, then I promise to leave you alone, no questions asked. </p>
+
+<p>All I’m asking for is your name and email so I can get in touch and explore this new option with you!</p>
+</div>
+
+<div class="btn-container">
+<button>SEND ME THE "TRICITY MORTGAGE INVESTMENT FUND" DETAILS!</button></div>
+
+</div>
+</div>
+</div>
+</div>
+
+
+         </main><!-- #main -->
+
+      
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
